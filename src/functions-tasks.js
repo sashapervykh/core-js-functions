9bea76d2e9942f677fb0e897b1e6f407ff747a7c
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2025-04-02 16:09:30 +0300

    feat: add getIdGeneratorFunction

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -190,8 +190,12 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let start = startFrom;
+  return () => {
+    start += 1;
+    return start - 1;
+  };
 }
 
 module.exports = {
